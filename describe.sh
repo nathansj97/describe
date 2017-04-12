e64af55f529b8326337b03084bbda661cc8ae7d2
nathansj97 <nathansj97@gmail.com>
2017-04-12 23:47:32 +0100

    Initial commit to add describe script.

---
+++ describe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IMAGE_SRC=$1
+URL=https://westus.api.cognitive.microsoft.com/vision/v1.0/describe
+API_KEY=11e09e236c35431792b628d1c745e7d1
+
+if [ -e "$IMAGE_SRC" ]
+then
+    echo Generating caption...
+    curl -Ss --request POST --header "Ocp-Apim-Subscription-Key: $API_KEY" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary "@$IMAGE_SRC" $URL | \
+    python3 -c "import sys, json; print(json.load(sys.stdin)['description']['captions'][0]['text'])"
+else
+    echo "Error: File does not exist: $IMAGE_SRC"
+fi
